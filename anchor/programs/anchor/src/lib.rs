b6bcd00be0f0f5497b4a356ccfedc17265e7b778
sarah-optimize <96051004+sarah-optimize@users.noreply.github.com>
2023-08-19 23:16:59 +0800

    Update project files

--- anchor/programs/anchor/src/lib.rs
+++ anchor/programs/anchor/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("BbqB5v7ukqDquJ1TaDTZfsYtuE51QLJZe76VGdc6xgZC");
+declare_id!("EozGe4eZCQE3gTuJwUT8ufM4e7EAVc4VSnjP2nHax7om");
 
 #[program]
 pub mod record_program {
@@ -8,7 +8,6 @@ pub mod record_program {
     
     pub fn add_record(ctx: Context<AddRecord>, text: String, amount: u64) -> Result<()> {
         let user_account = &mut ctx.accounts.record;
-        user_account.author = ctx.accounts.user.key();
         user_account.text = text;
         user_account.amount = amount;
         Ok(())
@@ -16,17 +15,17 @@ pub mod record_program {
 }
 
 #[derive(Accounts)]
+#[instruction()]
 pub struct AddRecord<'info> {
-    #[account(init, payer = user, space = 8 + 4 + 8)]
+    #[account(init, payer=authority, space = 9000 )]
     pub record: Account<'info, Record>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct Record {
-    pub author: Pubkey,
     pub text: String,
     pub amount: u64,
 }
